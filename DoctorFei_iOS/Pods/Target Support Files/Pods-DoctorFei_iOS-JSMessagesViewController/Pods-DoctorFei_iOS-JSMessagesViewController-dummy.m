e7ab11debbadc8a83966ccde8dc399bec75f6dd9
gujunjia <gujunjia@gmail.com>
2014-11-22 11:12:11 +0800

    cocoapod

---
+++ DoctorFei_iOS/Pods/Target Support Files/Pods-DoctorFei_iOS-JSMessagesViewController/Pods-DoctorFei_iOS-JSMessagesViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DoctorFei_iOS_JSMessagesViewController : NSObject
+@end
+@implementation PodsDummy_Pods_DoctorFei_iOS_JSMessagesViewController
+@end
---
+++ DoctorFei_iOS/Pods/Target Support Files/Pods-DoctorFei_iOS/Pods-DoctorFei_iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DoctorFei_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_DoctorFei_iOS
+@end
