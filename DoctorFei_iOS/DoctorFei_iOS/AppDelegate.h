cbb76269a7da9e49394615f39646f81fa8c02d05
gujunjia <gujunjia@gmail.com>
2014-11-22 11:09:21 +0800

    init

---
+++ DoctorFei_iOS/DoctorFei_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoctorFei_iOS
+//
+//  Created by GuJunjia on 14/11/22.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoctorFei_iOS/DoctorFei_iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoctorFei_iOS
+//
+//  Created by GuJunjia on 14/11/22.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
