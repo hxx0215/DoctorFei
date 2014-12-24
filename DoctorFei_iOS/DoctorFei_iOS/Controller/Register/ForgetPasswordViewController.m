9e963e6ea833731ff06222115fd246f6bcfc66cd
gujunjia <gujunjia@gmail.com>
2014-12-24 23:16:37 +0800

    忘记密码输入手机号码TextField外观变更

--- DoctorFei_iOS/DoctorFei_iOS/Controller/Register/ForgetPasswordViewController.m
+++ DoctorFei_iOS/DoctorFei_iOS/Controller/Register/ForgetPasswordViewController.m
@@ -69,6 +69,10 @@ static const NSTimeInterval kDuration = 60;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.phoneTextField.layer.borderWidth = 1.0f;
+    self.phoneTextField.layer.borderColor = UIColorFromRGB(0xA6A6A6).CGColor;
+    self.phoneTextField.layer.cornerRadius = 8.0f;
+    self.phoneTextField.layer.sublayerTransform = CATransform3DMakeTranslation(10, 0, 0);
     RAC(self.getCapthaButton, enabled) = [RACSignal combineLatest:@[self.phoneTextField.rac_textSignal] reduce:^(NSString *phone){
         return @(phone.length == 11 && timeCount == 0);
     }];
