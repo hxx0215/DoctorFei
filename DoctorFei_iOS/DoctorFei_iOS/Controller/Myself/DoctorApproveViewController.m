eb22a7a4628d7168f0266811c513639b9eade39f
hxx0215 <hxx0215@126.com>
2015-01-06 20:17:20 +0800

    我 - 医生资质认证界面

--- DoctorFei_iOS/DoctorFei_iOS/Controller/Myself/DoctorApproveViewController.m
+++ DoctorFei_iOS/DoctorFei_iOS/Controller/Myself/DoctorApproveViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "DoctorApproveViewController.h"
-
-@interface DoctorApproveViewController ()
+#import "ImageUtil.h"
+@interface DoctorApproveViewController ()<UIActionSheetDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *approveImage;
 
 @end
@@ -38,6 +38,35 @@
     [self.navigationController popViewControllerAnimated:YES];
 }
 - (IBAction)chooseApproveImage:(id)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"" delegate:self cancelButtonTitle:NSLocalizedString(@"取消", nil) destructiveButtonTitle:nil otherButtonTitles:NSLocalizedString(@"拍照", nil),NSLocalizedString(@"从手机相册选择", nil), nil];
+    [actionSheet showInView:self.view];
 }
-
+#pragma mark - UIActionSheetDelegate
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex{
+    NSUInteger sourceType = 0;
+    if (0==buttonIndex){
+        //拍照
+        sourceType = UIImagePickerControllerSourceTypeCamera;
+    }else if (1==buttonIndex){
+        //相册
+        sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }else{
+        return ;
+    }
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+        UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+        imagePickerController.delegate = self;
+        imagePickerController.allowsEditing = YES;
+        imagePickerController.sourceType = sourceType;
+        [self presentViewController:imagePickerController animated:YES completion:nil];
+    }
+}
+#pragma mark - UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    UIImage *image=[info objectForKey:UIImagePickerControllerEditedImage];
+    [picker dismissViewControllerAnimated:YES completion:^{
+        UIImage *cropImage = [ImageUtil imageWithImage:image scaledToSize:CGSizeMake(160.0f, 160.0f)];
+        [self.approveImage setImage:cropImage forState:UIControlStateNormal];
+    }];
+}
 @end
