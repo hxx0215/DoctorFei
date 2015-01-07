eeb5e08aacc7357e9cbd734706c11b16627a5d54
gujunjia <gujunjia@gmail.com>
2015-01-07 20:37:32 +0800

    加入患者端 空工程

---
+++ DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DoctorFei_Patient_iOS
+//
+//  Created by GuJunjia on 15/1/7.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DoctorFei_Patient_iOS
+//
+//  Created by GuJunjia on 15/1/7.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
