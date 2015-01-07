ab4bd0118f680cf8bacc77bd880a0c6624f7ded0
hxx0215 <hxx0215@126.com>
2015-01-07 22:28:54 +0800

    通讯录 - 二维码扫描以及相关库添加

--- DoctorFei_iOS/DoctorFei_iOS/Controller/Contact/QRCodeScanViewController.m
+++ DoctorFei_iOS/DoctorFei_iOS/Controller/Contact/QRCodeScanViewController.m
@@ -7,13 +7,16 @@
 //
 
 #import "QRCodeScanViewController.h"
-
-@interface QRCodeScanViewController ()
-
+#import <AVFoundation/AVFoundation.h>
+@interface QRCodeScanViewController ()<AVCaptureMetadataOutputObjectsDelegate>
+@property (strong, nonatomic) AVCaptureSession *session;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer *preview;
 @end
 
 @implementation QRCodeScanViewController
-
+{
+    dispatch_source_t timer;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -26,7 +29,11 @@
 - (IBAction)backButtonClicked:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
 }
-
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self setupCamera];
+}
 /*
 #pragma mark - Navigation
 
@@ -36,5 +43,94 @@
     // Pass the selected object to the new view controller.
 }
 */
+- (void)setupCamera{
+    // Device
+    AVCaptureDevice* device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    // Input
+    AVCaptureDeviceInput* input = [AVCaptureDeviceInput deviceInputWithDevice:device error:nil];
+    // Output
+    AVCaptureMetadataOutput* output = [[AVCaptureMetadataOutput alloc]init];
+    [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    
+    // Session
+    _session = [[AVCaptureSession alloc]init];
+    [_session setSessionPreset:AVCaptureSessionPresetHigh];
+    
+    if ([_session canAddInput:input]) {
+        [_session addInput:input];
+    }
+    
+    if ([_session canAddOutput:output]) {
+        [_session addOutput:output];
+    }
+    
+    // 条码类型 AVMetadataObjectTypeQRCode
+    output.metadataObjectTypes = @[AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode39Mod43Code,
+                                   AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code,
+                                   AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeQRCode, AVMetadataObjectTypeAztecCode];
+    
+    // Preview
+    self.preview =[AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    self.preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    [self.view.layer insertSublayer:self.preview atIndex:0];
+    self.preview.frame = self.view.bounds;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        [self startScanning];
+    });
+}
+- (void)startScanning;
+{
+    timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 0.03 * NSEC_PER_SEC, 0.0);
+    dispatch_source_set_event_handler(timer, ^{
+    });
+    dispatch_source_set_cancel_handler(timer, ^{
+        NSLog(@"cancel");
+    });
+    dispatch_resume(timer);
+    
+    [self.session startRunning];
+}
+- (void) stopScanning;
+{
+    dispatch_source_cancel(timer);
+    [self.session stopRunning];
+}
+#pragma mark AVCaptureMetadataOutputObjectsDelegate
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    [self stopScanning];
+    
+    NSString *stringValue = nil;
+    if ([metadataObjects count] > 0) {
+        AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex:0];
+        stringValue = metadataObject.stringValue;
+    }
+    
+    
+    if (stringValue && stringValue.length > 0) {
+        NSURL* url = [NSURL URLWithString:stringValue];
+        if ([[UIApplication sharedApplication] canOpenURL:url]) {
+            
+            [self showBrowserWithSting:stringValue];
+            
+        }else{
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+                [self showQRCodeResult:stringValue];
+            });
+        }
+    }else{
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+}
+- (void)showQRCodeResult:(NSString*)stringValue
+{
+    NSLog(@"%@",stringValue);
+}
 
+- (void)showBrowserWithSting:(NSString*)stringValue
+{
+//    NSLog(@"%@",stringValue);
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:stringValue]];
+}
 @end
