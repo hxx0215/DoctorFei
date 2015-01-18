3fbd3f6d9cdf7fb8ea4f45e13cd1425842148258
gujunjia <gujunjia@gmail.com>
2015-01-18 16:24:55 +0800

    我的主页- 文章详情

---
+++ DoctorFei_iOS/DoctorFei_iOS/Controller/Main/MyPageContentArticleDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyPageContentArticleDetailViewController.h
+//  DoctorFei_iOS
+//
+//  Created by GuJunjia on 15/1/18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyPageContentArticleDetailViewController : UIViewController
+
+@end
---
+++ DoctorFei_iOS/DoctorFei_iOS/Controller/Main/MyPageContentArticleDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  MyPageContentArticleDetailViewController.m
+//  DoctorFei_iOS
+//
+//  Created by GuJunjia on 15/1/18.
+//
+//
+
+#import "MyPageContentArticleDetailViewController.h"
+
+@implementation MyPageContentArticleDetailViewController
+
+@end
