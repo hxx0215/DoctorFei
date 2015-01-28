5a1741a871692219c6f6a5da7dff290ba74a4a75
hxx0215 <hxx0215@126.com>
2015-01-28 22:33:07 +0800

    聊天 - 会诊弹出

--- DoctorFei_iOS/DoctorFei_iOS/Controller/Contact/ContactDetailViewController.h
+++ DoctorFei_iOS/DoctorFei_iOS/Controller/Contact/ContactDetailViewController.h
@@ -5,12 +5,15 @@
 //  Created by GuJunjia on 14/12/4.
 //
 //
-
+typedef enum ContactDetailMode{
+    ContactDetailModeNormal,
+    ContactDetailModeConsultation
+} ContactDetailMode;
 #import <JSQMessages.h>
 @class Friends;
 
 @interface ContactDetailViewController : JSQMessagesViewController
 
 @property (nonatomic, strong) Friends *currentFriend;
-
+@property (nonatomic, assign) ContactDetailMode detailMode;
 @end
