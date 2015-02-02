29cec77d042f19192b9488c5cd3b992b140e9af0
hxx0215 <hxx0215@126.com>
2015-02-02 20:48:40 +0800

    个人资料界面调整

---
+++ DoctorFei_iOS/DoctorFei_iOS/View/ContactFriendDetailTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ContactFriendDetailTableViewCell.m
+//  DoctorFei_iOS
+//
+//  Created by hxx on 2/2/15.
+//
+//
+
+#import "ContactFriendDetailTableViewCell.h"
+
+@implementation ContactFriendDetailTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
