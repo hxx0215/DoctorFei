fb705534ad7356a635980b738fed787882f16d8a
liuxh532 <786995209@qq.com>
2015-03-03 21:37:49 +0800

    医生端 附近的人

--- DoctorFei_iOS/DoctorFei_iOS/View/ContactNearbyTableViewCell.m
+++ DoctorFei_iOS/DoctorFei_iOS/View/ContactNearbyTableViewCell.m
@@ -7,8 +7,14 @@
 //
 
 #import "ContactNearbyTableViewCell.h"
+#import "DoctorAPI.h"
+#import <MBProgressHUD.h>
+#import <UIImageView+WebCache.h>
 
 @implementation ContactNearbyTableViewCell
+{
+    NSDictionary *dict;
+}
 
 - (void)awakeFromNib {
     // Initialization code
@@ -20,4 +26,46 @@
     // Configure the view for the selected state
 }
 
+- (IBAction)addFriendClicked:(UIButton *)sender {
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
+    NSNumber *userId = [[NSUserDefaults standardUserDefaults]objectForKey:@"UserId"];
+    NSDictionary *params = @{
+                             @"usertype": dict[@"usertype"],
+                             @"doctorid": [userId stringValue],
+                             @"friendid": dict[@"userid"]
+                             };
+    [DoctorAPI addFriendWithParameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"addFriend: %@",responseObject);
+        NSDictionary *dic = [responseObject firstObject];
+        if ([dic[@"state"] integerValue]==1) {
+            UIImageView *completeImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo_prompt-01_pic.png"]];
+            hud.mode = MBProgressHUDModeCustomView;
+            hud.dimBackground = YES;
+            hud.customView = completeImage;
+        }
+        hud.labelText = dic[@"msg"];//NSLocalizedString(@"好友添加成功", nil);
+        [hud hide:YES afterDelay:2.0];
+        sender.enabled = NO;
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        hud.mode = MBProgressHUDModeText;
+        hud.labelText = @"错误";
+        hud.detailsLabelText = error.localizedDescription;
+        [hud hide:YES afterDelay:1.5f];
+    }];
+    
+    
+}
+
+-(void)setDataDic:(NSDictionary *)dic
+{
+    NSArray *tagImageArray = [NSArray arrayWithObjects:@"patient_tag.png",
+                              @"family-member_tag.png",
+                              @"dector_tag.png",nil];
+    dict = [dic copy];
+    self.nameLabel.text = dict[@"realname"];
+    [self.iconImage sd_setImageWithURL:dict[@"icon"] placeholderImage:[UIImage imageNamed:@"doctor-ranking_preinstall_pic.png"]];
+    self.addButton.enabled = ![dic[@"myfirend"] integerValue];
+    self.distanceLabel.text = [NSString stringWithFormat:@"%ldm",[dic[@"distance"] integerValue]];
+    [self.typeImage setImage:[UIImage imageNamed:[tagImageArray objectAtIndex:[dic[@"usertype"] integerValue]]]];
+}
 @end
