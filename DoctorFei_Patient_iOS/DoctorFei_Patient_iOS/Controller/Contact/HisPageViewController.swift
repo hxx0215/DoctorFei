0d7d0ad0fb2de21fdafbf7cbdda247c01018912e
hxx0215 <hxx0215@126.com>
2015-03-16 09:59:33 +0800

    他的主页

---
+++ DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/Controller/Contact/HisPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HisPageViewController.swift
+//  DoctorFei_Patient_iOS
+//
+//  Created by shadowPriest on 3/15/15.
+//
+//
+
+import UIKit
+
+class HisPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
