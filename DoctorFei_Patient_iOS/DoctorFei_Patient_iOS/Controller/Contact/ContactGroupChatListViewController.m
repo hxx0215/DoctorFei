99096adbdc4bbd2922fc27b997972fb29e4fe1bb
hxx0215 <hxx0215@126.com>
2015-03-24 19:09:35 +0800

    患者端 - 群聊列表

--- DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/Controller/Contact/ContactGroupChatListViewController.m
+++ DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/Controller/Contact/ContactGroupChatListViewController.m
@@ -8,22 +8,47 @@
 
 #import "ContactGroupChatListViewController.h"
 
-@interface ContactGroupChatListViewController ()
-
+@interface ContactGroupChatListViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *groupData;
 @end
 
 @implementation ContactGroupChatListViewController
-
+static NSString * const contactGroupChatListIdentifier = @"ContactGroupChatListIdentifier";
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.tableView.tableFooterView = [UIView new];
+    self.groupData = [NSMutableArray new];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self getGroupData];
+}
+- (IBAction)backButtonClicked:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
 
+- (void)getGroupData{
+    
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.groupData.count + 1;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:contactGroupChatListIdentifier];
+    if (!cell){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:contactGroupChatListIdentifier];
+    }
+    cell.textLabel.textColor = [UIColor greenColor];
+    cell.textLabel.text = @"新建群";
+    return cell;
+}
 /*
 #pragma mark - Navigation
 
