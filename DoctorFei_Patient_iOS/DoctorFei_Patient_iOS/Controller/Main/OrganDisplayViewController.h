9dacdd9fdc63f52b7ac415dd6295270ca05d903a
hxx0215 <hxx0215@126.com>
2015-03-28 21:50:05 +0800

    机构展示准备工作

--- DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/Controller/Main/OrganDisplayViewController.h
+++ DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/Controller/Main/OrganDisplayViewController.h
@@ -5,9 +5,13 @@
 //  Created by GuJunjia on 15/2/27.
 //
 //
-
+typedef enum OrganType{
+    OrganTypeShow,
+    OrganTypeOutstanding,
+    OrganTypeNursing
+}OrganType;
 #import <UIKit/UIKit.h>
 
 @interface OrganDisplayViewController : UIViewController
-
+@property (nonatomic, assign) OrganType type;
 @end
