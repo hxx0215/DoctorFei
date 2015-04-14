4b35467cd386adeb2501e30733f6867e37fa940e
hxx0215 <hxx0215@126.com>
2015-04-14 08:58:47 +0800

    机构、优秀、护工详细分享。昨天忘提交了~

--- DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/Controller/Main/OrganDisplayDetailViewController.m
+++ DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/Controller/Main/OrganDisplayDetailViewController.m
@@ -8,14 +8,23 @@
 
 #import "OrganDisplayDetailViewController.h"
 #import "MemberAPI.h"
+#import "ShareUtil.h"
+#import "MBProgressHUD.h"
+typedef void (^shareHide)(void);
 @interface OrganShareView :UIView
+@property (nonatomic,copy)shareHide hide;
 @end
 @implementation OrganShareView
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    self.hide();
+}
 @end
 @interface OrganDisplayDetailViewController ()
 - (IBAction)backButtonClicked:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextView *content;
-
+@property (weak, nonatomic) IBOutlet OrganShareView *shareView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *shareBottomConstraint;
+@property (strong, nonatomic) MBProgressHUD *hud;
 @end
 
 @implementation OrganDisplayDetailViewController
@@ -23,6 +32,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.shareView.hidden = YES;
+    typeof(self) __weak weakSelf = self;
+    self.shareView.hide = ^{
+       typeof(self) strongSelf = weakSelf;
+        [strongSelf showShare:NO];
+    };
+    self.shareBottomConstraint.constant = -240;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -71,6 +87,83 @@
     // Pass the selected object to the new view controller.
 }
 */
+- (void)showShare:(BOOL)flag{
+    if (flag){
+        self.shareBottomConstraint.constant = 0;
+        self.shareView.hidden = NO;
+    }
+    else
+        self.shareBottomConstraint.constant = -240;
+    [self.shareView setNeedsUpdateConstraints];
+    [UIView animateWithDuration:0.5 animations:^{
+        [self.shareView layoutIfNeeded];
+    }completion:^(BOOL finished){
+        self.shareView.hidden = !flag;
+    }];
+}
+- (IBAction)showShareView:(id)sender {
+    [self showShare:YES];
+}
+- (IBAction)shareAction:(UIButton *)sender {
+    NSDictionary *dic = @{@"content": self.content.text,
+                          @"vc" : self,
+                          @"title" : @" ",
+                          @"url" : @""};
+    
+    ShareType sharetype = ShareTypeSinaWeibo;
+    switch (sender.tag) {
+        case 300:
+        {
+            sharetype = ShareTypeSinaWeibo;
+        }
+            break;
+         case 301:
+            sharetype = ShareTypeTencentWeibo;
+            break;
+        case 302:
+            sharetype = ShareTypeWeixiSession;
+            break;
+        case 303:
+            sharetype = ShareTypeQQSpace;
+            break;
+        case 304:
+            sharetype = ShareTypeQQ;
+            break;
+        case 305:
+            sharetype = ShareTypeWeixiTimeline;
+            break;
+        case 306:
+            sharetype = ShareTypeSMS;
+            break;
+        default:
+            sharetype = ShareTypeSinaWeibo;
+            break;
+    }
+    [[ShareUtil sharedShareUtil] shareTo:sharetype content:dic complete:^(ShareType type, SSResponseState state, id<ISSPlatformShareInfo> statusInfo, id<ICMErrorInfo>error, BOOL end){
+        if (type == ShareTypeSMS)
+            return ;
+        switch (state) {
+            case SSResponseStateBegan:
+                self.hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+                self.hud.labelText = @"发布中";
+                break;
+            case SSResponseStateSuccess:
+                self.hud.mode = MBProgressHUDModeText;
+                self.hud.labelText = @"发布成功";
+                [self.hud hide:YES afterDelay:0.5];
+                break;
+            case SSResponseStateFail:
+                self.hud.mode = MBProgressHUDModeText;
+                self.hud.labelText = [NSString stringWithFormat:@"发布失败:%@",error];
+                [self.hud hide:YES afterDelay:1.5];
+            case SSResponseStateCancel:
+                self.hud.labelText = @"已取消";
+                [self.hud  hide:YES];
+            default:
+                break;
+        }
+    }];
+}
 
 - (IBAction)backButtonClicked:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
