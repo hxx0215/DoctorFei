6ff5f5333d63384dabfaf5428d7a69ecbb0db92c
hxx0215 <hxx0215@126.com>
2015-04-18 18:08:31 +0800

    通知以及swift更新

--- DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/View/NotificationTableViewCell.m
+++ DoctorFei_Patient_iOS/DoctorFei_Patient_iOS/View/NotificationTableViewCell.m
@@ -10,12 +10,14 @@
 @interface NotificationTableViewCell()
 @property (weak, nonatomic) IBOutlet UILabel *contentLabel;
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UIView *backView;
 
 @end
 @implementation NotificationTableViewCell
 
 - (void)awakeFromNib {
     // Initialization code
+    self.backView.layer.cornerRadius = 4.0;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
@@ -25,5 +27,9 @@
 }
 - (void)setCellData:(NSDictionary *)data{
     self.contentLabel.text = data[@"title"];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy年MM月dd日";
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:[data[@"addtime"] doubleValue]];
+    self.dateLabel.text = [formatter stringFromDate:date];
 }
 @end
